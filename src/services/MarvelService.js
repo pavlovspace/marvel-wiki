032c90f4a9f0c39921e2c4284e0306ad9955793a
Paul Pavlov <pavlov1space@gmail.com>
2024-04-06 15:55:11 +0200

    implemented Data transformation and random character component

---
+++ src/services/MarvelService.js
@@ -0,0 +1,36 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/'
+    _apiKey = 'apikey=5c038bd6ce84d9ec6fa43f3dab859913'
+
+    getResource = async (url) => {
+        let res = await fetch(url)
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
+
+        return await res.json()
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+        return res.data.results.map(this._transformCharacter)
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+        return this._transformCharacter(res)
+    }
+
+    _transformCharacter = (res) => {
+        return {
+            name: res.data.results[0].name,
+            description: res.data.results[0].description,
+            thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
+            homepage: res.data.results[0].urls[0].url,
+            wiki: res.data.results[0].urls[1].url,
+        }
+    }
+}
+
+export default MarvelService
